266d7fb2fd4f28e4a1b119c7813d4b9bae4c09a0
OliveiraaDev <valdelane.l@hotmail.com>
2025-10-22 22:06:01 -0300

    alterações no CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { Fragment } from "react/jsx-runtime";
-import { Heading } from "./components/Heading";
 import './styles/tema.css';
 import './styles/global.css';
 
- export function App () {
-    console.log('oi');
+export function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <section>
+            <p>
+              LOGO
+            </p>
+          </section>
+        </div>
+      </div>
+           <div className="container">
+        <div className="content">
+          <section>
+            <p>
+              MENU
+            </p>
+          </section>
+        </div>
+      </div>
 
-    return (
-    <Fragment>
-        <Heading/>
-        
-            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid consequatur ab vitae similique molestiae adipisci quos enim voluptatibus, assumenda perferendis dolorem nulla unde dolores provident! Culpa incidunt illum qui dignissimos?</p>
-    </Fragment>);
-
+           <div className="container">
+        <div className="content">
+          <section>
+            <p>
+              FORMULÁRIO
+            </p>
+          </section>
+        </div>
+      </div>
+           
+    </>
+  );
 }
